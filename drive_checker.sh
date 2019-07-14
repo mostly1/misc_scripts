813a6677eee57606df5bfc2d521c9908c9f51924
Jeff Mazzone <mostly1@users.noreply.github.com>
2019-07-13 21:50:23 -0500

    Update drive_checker.sh

--- drive_checker.sh
+++ drive_checker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+user=""
 for i in $(cat full_list.txt); do
 #for i in $(grep "address =" ${1}/*.conf | awk -F "\"" '{print $2}'); do
 
-if ssh -A lacadmin@${i} '[ -d /opt/MegaRAID/MegaCli/ ]' ;then
-  ssh -A lacadmin@${i} hostname
+if ssh -A ${user}@${i} '[ -d /opt/MegaRAID/MegaCli/ ]' ;then
+  ssh -A ${user}@${i} hostname
   echo "megaraid exists. Lets try to check for errors.."
   echo "Checking ip ${i} "
-  errors=$(ssh -A lacadmin@${i} sudo /opt/MegaRAID/MegaCli/lsi.sh errors | grep -i "error" -B1)
-  num_drives=$(ssh -A lacadmin@${i} sudo /opt/MegaRAID/MegaCli/lsi.sh drives | wc -l)
+  errors=$(ssh -A ${user}@${i} sudo /opt/MegaRAID/MegaCli/lsi.sh errors | grep -i "error" -B1)
+  num_drives=$(ssh -A ${user}@${i} sudo /opt/MegaRAID/MegaCli/lsi.sh drives | wc -l)
   if [ -n "${errors}" ]; then
       echo "${errors}"
       echo "Total number of Drives for ip: ${i} " ${num_drives}
